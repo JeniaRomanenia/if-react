98565ef4838213346c917ec036cf1501b178ca5b
JeniaRomanenia <jenia02041985@gmail.com>
2021-03-09 22:20:20 +0300

    changed component HomesGuestsLoves.js and App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,12 @@ import React from 'react';
 //components
 import HomesGuestsLoves from "./HomesGuestsLoves";
 
-//constants
-import {hotels} from "../constants/content";
-
 //styles
 import '../styles/components/App.css';
 
 function App() {
   return (
-      <HomesGuestsLoves hotels={hotels}/>
+      <HomesGuestsLoves />
   );
 }
 
--- src/components/HomesGuestsLoves.js
+++ src/components/HomesGuestsLoves.js
@@ -4,16 +4,21 @@ import PropTypes from 'prop-types';
 //styles
 import '../styles/components/HomesGuestsLoves.css';
 
+//constants
+import {hotels} from "../constants/content";
+
 //components
 import Hotel from "./Hotels.js";
 
-const HomesGuestsLoves = (props) => (
+const HomesGuestsLoves = ( ) => (
     <div className="homes">
         <div className="container">
             <h2 className= "h2-text">Homes guests loves</h2>
             <div className="row">
-                    {props.hotels.map(hotel => (
-                        <Hotel key={hotel.id} hotel={hotel}/>
+                    {hotels.map(hotel => (
+                        <Hotel
+                            key={hotel.id}
+                            hotel={hotel}/>
                     ))}
             </div>
         </div>
