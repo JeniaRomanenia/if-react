e43c80fb0abc8b49725ea5398c74481712185e4e
Jenia Romanenia <71782573+JeniaRomanenia@users.noreply.github.com>
2021-03-10 09:07:57 +0300

    Made a section "Homes guests loves" and split it into components. (#1)
    * Made a section "Homes guests loves" and split it into components.
    
    * Сorrected comments
    
    * Added prop-types
    
    * changed component HomesGuestsLoves.js and App.js

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//components
+import HomesGuestsLoves from "./HomesGuestsLoves";
+
+//styles
+import '../styles/components/App.css';
+
+function App() {
+  return (
+      <HomesGuestsLoves />
+  );
+}
+
+export default App;
+
---
+++ src/components/HomesGuestsLoves.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+//styles
+import '../styles/components/HomesGuestsLoves.css';
+
+//constants
+import {hotels} from "../constants/content";
+
+//components
+import Hotel from "./Hotels.js";
+
+const HomesGuestsLoves = ( ) => (
+    <div className="homes">
+        <div className="container">
+            <h2 className= "h2-text">Homes guests loves</h2>
+            <div className="row">
+                    {hotels.map(hotel => (
+                        <Hotel
+                            key={hotel.id}
+                            hotel={hotel}/>
+                    ))}
+            </div>
+        </div>
+    </div>);
+
+HomesGuestsLoves.propTypes = {
+    hotels: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
+export default HomesGuestsLoves;
